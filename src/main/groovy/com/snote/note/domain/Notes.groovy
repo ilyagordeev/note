936b9f91930cee915dec68a8e5c3991dfd51644e
Ilya Gordeev <ilya.tanat@gmail.com>
2019-12-20 06:33:40 +0500

    set note column length to 4000

--- src/main/groovy/com/snote/note/domain/Notes.groovy
+++ src/main/groovy/com/snote/note/domain/Notes.groovy
@@ -4,6 +4,7 @@ package com.snote.note.domain
 import org.hibernate.annotations.CreationTimestamp
 
 import javax.persistence.CascadeType
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -21,6 +22,7 @@ class Notes {
 
     String heading
     @NotNull
+    @Column(length = 4000)
     String note
     @CreationTimestamp
     Date timestamp
